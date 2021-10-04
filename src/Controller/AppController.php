84c0c2cb3caeb4c310db3ab7b98b4ca10a3d97d9
ced <cedricnoisette9@gmail.com>
2021-10-04 11:17:22 +0200

    feat/user auth

---
+++ src/Controller/AppController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/app")
+ */
+class AppController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_index")
+     */
+    public function index(): Response
+    {
+        return $this->render('app/index.html.twig');
+    }
+}
---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    private $passwordHasher;
+
+        public function __construct(UserPasswordHasherInterface $passwordHasher)
+        {
+            $this->passwordHasher = $passwordHasher;
+        }
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setEmail('user@mail.com');
+        $user->setPassword($this->passwordHasher->hashPassword($user, 'pass'));
+        $user->setRoles(['ROLE_USER']);
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
