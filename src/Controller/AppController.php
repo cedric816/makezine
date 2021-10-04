512910e6599a67ecd7edbfa0404ed17dbc282cc0
ced <cedricnoisette9@gmail.com>
2021-10-04 18:26:24 +0200

    install encore and bootstrap

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\FanzineRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,11 +12,35 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class AppController extends AbstractController
 {
+    private $zineRepo;
+
+    public function __construct(FanzineRepository $zineRepo)
+    {
+        $this->zineRepo = $zineRepo;
+    }
     /**
      * @Route("/", name="app_index")
      */
     public function index(): Response
     {
-        return $this->render('app/index.html.twig');
+        $fanzines = $this->getUser()->getFanzines();
+        return $this->render('app/index.html.twig',[
+            "fanzines" => $fanzines
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="edit_zine")
+     */
+    public function edit($id): Response
+    {
+        $fanzines = $this->getUser()->getFanzines();
+        $selectedZine = $this->zineRepo->find($id);
+        $pages = $selectedZine->getPages();
+        return $this->render('app/index.html.twig',[
+            "fanzines" => $fanzines,
+            "selectedZine" => $selectedZine,
+            "pages" => $pages
+        ]);
     }
 }
